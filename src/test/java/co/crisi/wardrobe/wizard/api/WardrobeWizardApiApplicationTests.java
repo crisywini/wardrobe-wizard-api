becc78f4e7510cf12e1a43a56e8f3dbf3b986fc2
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2024-08-07 10:54:43 -0500

    Init project

---
+++ src/test/java/co/crisi/wardrobe/wizard/api/WardrobeWizardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.crisi.wardrobe.wizard.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeWizardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
