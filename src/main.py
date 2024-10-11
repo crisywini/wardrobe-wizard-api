a47d70020b8b4a7ede82e2528c72d98d38c21d4e
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2024-10-10 21:41:44 -0500

    Added upload image

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -7,3 +8,11 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+
+@app.post("/upload/")
+async def upload_image(image: UploadFile = File(...)):
+    file_location = f"{image.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(image.file.read())
+    
+    return JSONResponse(content={"filename": image.filename})
