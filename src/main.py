203f612d3b4a5826eedc7694ff6a7826dbe08e44
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2025-01-12 14:47:10 -0500

    Add cors to API

--- src/main.py
+++ src/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from router import item_router, outfit_router
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
 app.include_router(item_router.router)
 app.include_router(outfit_router.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,  
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 app.mount("/images", StaticFiles(directory="static/images"), name="images")
 
