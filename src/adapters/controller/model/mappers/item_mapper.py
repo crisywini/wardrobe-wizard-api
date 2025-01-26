afc090615a423186d828d695f1dbe1a4e3b95a38
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2025-01-25 22:51:53 -0500

    Add item mapper and refactor item controller

--- src/adapters/controller/model/mappers/item_mapper.py
+++ src/adapters/controller/model/mappers/item_mapper.py
@@ -1,6 +1,6 @@
 from src.adapters.controller.model.item_dto import ItemDto
 from src.domain.builders.item_builder import ItemBuilder
-
+from src.domain.entities.item import Item
 
 def map_to_entity(item_dto: ItemDto):
     return ItemBuilder.set_id(item_dto.id).set_name(item_dto.name).set_category(
@@ -8,3 +8,16 @@ def map_to_entity(item_dto: ItemDto):
         item_dto.color).set_style(item_dto.style).set_brand(item_dto.brand).set_season(
         item_dto.season).set_image_url(
         item_dto.image_url).build()
+
+
+def map_to_dto(item: Item):
+    return ItemDto(
+        id=str(item.id),
+        name=item.name,
+        category=item.category,
+        color=item.color,
+        style=item.style,
+        brand=item.brand,
+        season=item.season,
+        image_url=item.image_url
+    )
