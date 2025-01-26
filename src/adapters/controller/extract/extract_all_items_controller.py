37abf7a8dbc9a90e3ec5c62047393ae054ccb09f
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2025-01-25 23:05:53 -0500

    Add find all items controller

---
+++ src/adapters/controller/extract/extract_all_items_controller.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from src.adapters.gateway.mongodb.find.find_all_items_mongodb_gateway import FindAllItemsMongoDBGateway
+from src.adapters.gateway.mongodb.mongo_client_config import get_mongodb_instance
+from src.use_cases.find.find_all_items_use_case import FindAllItemsUseCase
+
+router = APIRouter()
+mongodb_instance = get_mongodb_instance()
+
+find_all_items_mongodb_gateway = FindAllItemsMongoDBGateway(mongodb_instance)
+find_all_items_use_case = FindAllItemsUseCase(find_all_items_mongodb_gateway)
+
+@router.get("/items")
+def get_items():
+    return find_all_items_use_case.run()
