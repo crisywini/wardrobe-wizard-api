d220cf4387a83d7852766866bec266df999400af
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2025-01-25 22:14:37 -0500

    Add find all outfits with mongo gateway

---
+++ src/adapters/gateway/mongodb/find/find_all_outfits_mongodb_gateway.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+from src.domain.builders.outfit_builder import OutfitBuilder
+from src.ports.gateways.find.find_all_outfits_gateway import FindAllOutfitsGateway
+
+
+class FindAllOutfitsMongoDBGateway(FindAllOutfitsGateway):
+    def __init__(self, mongo_client: MongoClient):
+        self.mongo_client = mongo_client
+        self.collection_name = "outfits"
+        self.collection = mongo_client[self.collection_name]
+
+    def run(self):
+        outfits_mongodb = self.collection.find()
+        outfits = list(
+            map(lambda outfit: OutfitBuilder.set_id(outfit.get("id")).set_name(outfit.get("name")).set_category(
+                outfit.get("category")).set_items(outfit.get("items")).build(), outfits_mongodb))
+        return outfits
