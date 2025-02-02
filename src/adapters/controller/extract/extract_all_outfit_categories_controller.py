576d3a035321414d26c346ee79c6c79b633d5d3c
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2025-02-01 22:20:19 -0500

    Refactor imports in all categories endpoint Closes #26

--- src/adapters/controller/extract/extract_all_outfit_categories_controller.py
+++ src/adapters/controller/extract/extract_all_outfit_categories_controller.py
@@ -1,14 +1,8 @@
-from typing import Optional
-
 from fastapi import APIRouter, Query
 
-from src.adapters.gateway.mongodb.find.find_all_outfits_by_category_mongodb_gateway import \
-    FindAllOutfitsByCategoryMongoDBGateway
 from src.adapters.gateway.mongodb.find.find_all_outfits_mongodb_gateway import FindAllOutfitsMongoDBGateway
 from src.adapters.gateway.mongodb.mongo_client_config import get_mongodb_instance
 from src.use_cases.find.find_all_outfit_categories_use_case import FindAllOutfitCategoriesUseCase
-from src.use_cases.find.find_all_outfits_by_category_use_case import FindAllOutfitsByCategoryUseCase
-from src.use_cases.find.find_all_outfits_use_case import FindAllOutfitsUseCase
 
 router = APIRouter()
 mongodb_instance = get_mongodb_instance()
