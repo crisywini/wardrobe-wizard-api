e6563638caf2daafaab53ad60512f766cdce281f
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2025-02-08 00:13:24 -0500

    Add Save outfit test including default image

--- src/use_cases/save/save_outfit_use_case.py
+++ src/use_cases/save/save_outfit_use_case.py
@@ -1,4 +1,4 @@
-from src.adapters.controller.config.constants import URL_IMAGE_DEFAULT_FE_PATH
+import src.adapters.controller.config.constants as constant
 from src.ports.gateways.save.save_outfit_gateway import SaveOutfitGateway
 from src.domain.entities.outfit import Outfit
 from src.use_cases.concatenate_images.concatenate_images_vertically_use_case import ConcatenateImagesVerticallyUseCase
@@ -13,12 +13,9 @@ class SaveOutfitUseCase:
         shirt = list(filter(lambda i: i["category"] == 'shirts', outfit.items))[0]
         pants = list(filter(lambda i: i["category"] == 'pants', outfit.items))[0]
         shoes = list(filter(lambda i: i["category"] == 'shoes', outfit.items))[0]
-        print(shirt)
-        print(pants)
-        print(shoes)
         file_name = outfit.name + ".png"
         self.concatenate_images_vertically_use_case.run([shirt["image_url"], pants["image_url"], shoes["image_url"]],
-                                                        f"{URL_IMAGE_DEFAULT_FE_PATH}/{file_name}")
-        outfit.default_image_url = f"{URL_IMAGE_DEFAULT_FE_PATH}/{file_name}"
+                                                        f"{constant.URL_IMAGE_DEFAULT_FE_PATH}/{file_name}")
+        outfit.default_image_url = f"{constant.URL_IMAGE_DEFAULT_FE_PATH}/{file_name}"
 
         return self.gateway.run(outfit)
---
+++ test/use_cases/save/save_outfit_use_case_test.py
@@ -0,0 +1,54 @@
+import pytest
+from PIL.ImageChops import constant
+from pymongo import MongoClient
+from testcontainers.mongodb import MongoDbContainer
+
+import src.adapters.controller.config.constants as constant
+from src.adapters.gateway.mongodb.save.save_item_mongdb_gateway import SaveItemMongoDBGateway
+from src.adapters.gateway.mongodb.save.save_outfit_mongodb_gateway import SaveOutfitMongoDBGateway
+from src.domain.builders.item_builder import ItemBuilder
+from src.domain.builders.outfit_builder import OutfitBuilder
+from src.use_cases.save.save_outfit_use_case import SaveOutfitUseCase
+
+import os
+
+
+@pytest.fixture(scope="module")
+def mongodb_container():
+    with MongoDbContainer("mongo:latest") as mongo:
+        yield mongo.get_connection_url()
+
+
+def test_save_outfit_with_correct_items_will_create_default_image(mongodb_container):
+    constant.URL_IMAGE_DEFAULT_FE_PATH = os.path.join(os.path.dirname(__file__), "../../resources")
+    client = MongoClient(mongodb_container)
+    db = client["wardrobe_db"]
+    gateway = SaveOutfitMongoDBGateway(db)
+    use_case = SaveOutfitUseCase(gateway)
+
+    shirt, pants, shoes = save_items(db)
+
+    outfit = OutfitBuilder().set_name("Cool American outfit").set_category("gym").set_items(
+        [shirt.to_dict(), pants.to_dict(), shoes.to_dict()]).build()
+    saved_outfit = use_case.run(outfit)
+
+    assert saved_outfit is not None
+
+
+def save_items(db):
+    gateway = SaveItemMongoDBGateway(db)
+
+    shirt = ItemBuilder().set_name("Green true shirt").set_category("shirts").set_color("green").set_style(
+        "sportive").set_brand("True").set_season("2024").set_image_url(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "../../resources/true_shirt.png"))).build()
+    pants = ItemBuilder().set_name("Blue jeans").set_category("pants").set_color("blue").set_style(
+        "Jeans").set_brand("nike").set_season("2024").set_image_url(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "../../resources/jeans_koaj.png"))).build()
+    shoes = ItemBuilder().set_name("white shoes").set_category("shoes").set_color("white").set_style(
+        "sportive").set_brand("new balance").set_season("2024").set_image_url(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "../../resources/new_balance_shoes.png"))).build()
+
+    shirt_saved = gateway.run(shirt)
+    pants_saved = gateway.run(pants)
+    shoes_saved = gateway.run(shoes)
+    return shirt_saved, pants_saved, shoes_saved
